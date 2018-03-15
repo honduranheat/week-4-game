d2a43decbec4a1c855d61f6a16fcaef286de22b3
Justin <jwhittington92@yahoo.com>
2018-03-15 18:38:54 -0400

    week-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,8 @@ Create Variables
     -Variable for a random number that shows on the screen between 19-120 
         -Random Number does not show until first click
     -Each crystal will have hidden value between 1-12
+Function that adds images to buttons
+
 When a crystal is clicked, it will add the crystal value to the score
     -Player wins when score matches random number
     -Player loses when score exceeds random number
@@ -16,31 +18,47 @@ When a crystal is clicked, it will add the crystal value to the score
 */////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
 //Variables
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var score = 0;
 var wins = 0;
 var losses = 0;  
+
     //Main Random Value
 var lotteryValue = Math.floor(Math.random() * 120) + 19;
+
     //Random Crystal Values
 var crystalValue1 = Math.floor(Math.random() * 12) + 1;
 var crystalValue2 = Math.floor(Math.random() * 12) + 1;
 var crystalValue3 = Math.floor(Math.random() * 12) + 1;
 var crystalValue4 = Math.floor(Math.random() * 12) + 1;
-    //Random Pictures for Buttons
-var images = ['../images/hamster1.jpeg','../images/hamster2.jpeg','../images/hamster3.jpeg','../images/hamster4.jpeg']
-var randomImg1 = images[Math.floor(Math.random() * images.length)];
-var randomImg2 = images[Math.floor(Math.random() * images.length)];
-var randomImg3 = images[Math.floor(Math.random() * images.length)];
-var randomImg4 = images[Math.floor(Math.random() * images.length)];
 
-document.getElementsByClassName("btn1").randomImg1;
-document.getElementsByClassName("btn2").randomImg2;
-document.getElementsByClassName("btn3").randomImg3;
-document.getElementsByClassName("btn4").randomImg4;
+    /*
+    //Random Pictures for Buttons COME BACK TO THIS USING CSS RIGHT NOW
+    var images = ['../images/hamster1.jpg','../images/hamster2.jpg','../images/hamster3.jpg',
+                '../images/hamster4.jpg','../images/hamster5.jpg','../images/hamster6.jpg','../images/hamster7.jpg','../images/hamster8.jpg']
+                var randomImg1 = images[Math.floor(Math.random() * images.length)];
+                var randomImg2 = images[Math.floor(Math.random() * images.length)];
+                var randomImg3 = images[Math.floor(Math.random() * images.length)];
+                var randomImg4 = images[Math.floor(Math.random() * images.length)];
+    */
 
 
+//jQuery
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function() {
+
+    
+        
+    $("button").on("click", function () {
+        $(".btn1").val(crystalValue2);
+        $(".btn2").val(crystalValue2);    
+        $(".btn3").val(crystalValue3);
+        $(".btn4").val(crystalValue4);   
+             
+    }); 
+});
 
 console.log(lotteryValue);
 console.log(crystalValue1);
